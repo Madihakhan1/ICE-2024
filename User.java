4b96d163875b99ec0cf20c0d67b78ffde167408b
Madihakhan1 <144325902+Madihakhan1@users.noreply.github.com>
2024-05-07 20:52:11 +0200

    ClassDiagram1.1.puml & Userklasseupdateret

--- User.java
+++ User.java
@@ -1,2 +1,44 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class User {
+
+    private String userName;
+    private String userPassword;
+    private String address;
+
+    private List<String> food;
+    private List<String> orders;
+
+
+    public User(String userName, String userPassword, String address, List<String>food, List<String>orders){
+        this.userName = userName;
+        this.userPassword = userPassword;
+        this.address = address;
+        this.food = new ArrayList<>();
+        this.orders = new ArrayList<>();
+
+    }
+
+    public String getUserName(){
+        return userName;
+    }
+
+    public String getUserPassword(){
+        return userPassword;
+    }
+
+    public List<String>getFood(){
+
+        return null;
+
+    }
+
+    public List<String>getOrders(){
+        return null;
+    }
+
+
+
+
 }
