2cc6141288a18b50fcf6bf455f06963abe7065ca
Madihakhan1 <144325902+Madihakhan1@users.noreply.github.com>
2024-05-08 12:21:05 +0200

    opdateret

--- Order.java
+++ Order.java
@@ -11,12 +11,25 @@ public String placeOrders(){
     return placeOrders();
 }
 
-public String displayOrder(){
-    return displayOrder();
+public void displayOrder(){
+
+        for (int i = 0; i < orders.size(); i++) {
+            Restaurant order = orders.get(i);
+            System.out.println("Order " + (i + 1) + ":");
+
+            for (Restaurant item : order.getItems()) {
+                System.out.println("- " + item.getName() + ": $" + item.getPrice());
+                // You can include more details like quantity, special instructions, etc. if needed
+            }
+            System.out.println("Total Price: $" + order.getTotalPrice());
+
+        }
 }
 
-public String deleteOrders(){
-    return deleteOrders();
+public void deleteOrders(){
+
+  orders.clear();
+
 }
 
 public String toString(){
